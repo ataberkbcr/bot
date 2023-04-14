5ab8b91327da8ded54a3b1b00525f83ce03dccad
ataberkbcr <124673426+ataberkbcr@users.noreply.github.com>
2023-04-14 10:35:42 +0300

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+
+$URL = "https://uptocoin.my.id/fp/faucet.php?address=DK85RHHRVYsB8obb1gEeoVaAe3QQHz92ti&currency=DOGE&key=1d625a83b09f948b83c04110b1ba763d";
+
+
+$cookie = "a=43rhg3VvLV5ELlOibwsrv52V4EDToywE; _popfired_expires=Invalid%20Date; _popfired=553; lastOpenAt_=1640409660685; token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYcaqPwFhxqo_gAGBAsAAICiNObsb-D5vleDNy1iYC6qvuBJLxxOxcu4hCIcusMGzwQBHMEUCIDpCMW23ut3FHUnMB9ILei_jNpnc_UhhqhH-LXqVX3YqAiEAirQZ4StQNF7aKJh-pNe38puRLzB0REs5pyGUmqDbEfU; _popprepop=1";
